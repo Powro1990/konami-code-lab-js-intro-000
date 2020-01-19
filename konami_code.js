c423fa27718ce978d9a8dd77360272890a4f2d3e
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-19 18:17:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,19 @@ const codes = [
 ];
 
 function init() {
-  document.body.addEventListener("keydown", (event) => {
-    
-  })
+  document.body.addEventListener("keydown",function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
